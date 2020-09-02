854c3eeb5cc445ec5cadae33aec5989191d546e3
Dr.C <dclinkenbeard@csumb.edu>
2020-09-01 17:42:16 -0700

    Made tests more efficient

--- app/src/test/java/com/daclink/drew/cst438fa2002_inclassgitdemo/UtilsTest.java
+++ app/src/test/java/com/daclink/drew/cst438fa2002_inclassgitdemo/UtilsTest.java
@@ -1,5 +1,6 @@
 package com.daclink.drew.cst438fa2002_inclassgitdemo;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Random;
@@ -8,17 +9,26 @@ import static org.junit.Assert.*;
 
 public class UtilsTest {
 
-    @Test
-    public void adderTest(){
+    int x;
+    int y;
+
+    @Before
+    public void setup() {
         Random random = new Random();
-        int x = random.nextInt();
-        int y = random.nextInt();
-        assertEquals(x+y, Utils.adder(x,y));
+        x = random.nextInt();
+        y = random.nextInt();
     }
 
     @Test
-    public void subtractorTest(){
-        assertEquals(3-2, Utils.subtractor(3,2));
+    public void adderTest() {
+        System.out.println("Testing adder with " + x + " and " + y);
+        assertEquals(x + y, Utils.adder(x, y));
+    }
+
+    @Test
+    public void subtractorTest() {
+        System.out.println("Testing Subtractor with " + x + " and " + y);
+        assertEquals(x - y, Utils.subtractor(x, y));
     }
 
 
